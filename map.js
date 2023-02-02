cfb075be51f315c100888f997b09fe4ff67c5bea
Nishant3105 <nishantkumar2522@gmail.com>
2023-02-02 15:56:43 +0530

    map

---
+++ map.js
@@ -0,0 +1,13 @@
+const arr=['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon']
+
+const transformedArr=arr.map((ele)=>{
+   if(ele===' ') return 'empty string'
+   else return ele
+})
+
+console.log(transformedArr)
+
+//Here hobbies is a const variable for an array it doesnot show error because it just stores the reference of that array
+//if we try to change the content inside it then it wont throw any error.
+
+
