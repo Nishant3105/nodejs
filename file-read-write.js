33e94b36f9befee4088e1b63816b9fd6d74fd0c3
Nishant3105 <nishantkumar2522@gmail.com>
2023-02-03 17:40:10 +0530

    file read write

---
+++ file-read-write.js
@@ -0,0 +1,48 @@
+const http=require('http')
+const fs=require('fs')
+const path=require('path')
+
+const server=http.createServer((req,res)=>{
+   const url=req.url
+   const method=req.method
+   if(url === '/'){
+        //const Filepath=path.join(__dirname,"message.txt")
+        fs.readFile("message.txt",'utf8',function(err, data) {
+            if (err) {
+                console.log(err);
+            }
+        const message=data
+        console.log(message)
+        res.write('<html>')
+        res.write(`<body>${message}</body>`)
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button>submit</button></input></form></body>')
+        res.write('</html>')
+        return res.end()
+        })
+    }
+    else if(url === '/message' && method==="POST"){
+        const body=[]
+        req.on('data',(chunk)=>{
+           body.push(chunk)
+        })
+        return req.on('end',()=>{
+            const parsedBody=Buffer.concat(body).toString()
+            const message=parsedBody.split('=')[1]
+            fs.writeFile('message.txt', message, err=>{
+                res.statusCode=302
+                res.setHeader('Location','/')
+                //upar ke dono res.writeHead se bhi ho jaega res.writeHead(statuscode,{})
+                return res.end()
+            })
+        })
+    }
+    else{
+        res.setHeader('Content-Type','text/html')
+        res.write('<html>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button>submit</button></input></form></body>')
+        res.write('</html>')
+        res.end()
+    }
+})
+
+server.listen(3010)
