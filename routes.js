21d4637a0e7989d54b3ca42e625368e113c0d3fb
Nishant3105 <nishantkumar2522@gmail.com>
2023-02-04 16:38:20 +0530

    routes

--- routes.js
+++ routes.js
@@ -1,55 +1,45 @@
 const fs=require('fs')
 
-var requestHandlers = (req,res)=>{   
-   const url=req.url
-   const method=req.method
-   if(url === '/'){
-        //const Filepath=path.join(__dirname,"message.txt")
-        fs.readFile("message.txt",'utf8',function(err, data) {
-            if (err) {
-                console.log(err);
-            }
-        const message=data
-        console.log(message)
-        res.write('<html>')
-        res.write(`<body>${message}</body>`)
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button>submit</button></input></form></body>')
-        res.write('</html>')
-        return res.end()
-        })
-    }
-    else if(url === '/message' && method==="POST"){
-        const body=[]
-        req.on('data',(chunk)=>{
-           body.push(chunk)
-        })
-        return req.on('end',()=>{
-            const parsedBody=Buffer.concat(body).toString()
-            const message=parsedBody.split('=')[1]
-            fs.writeFile('message.txt', message, err=>{
-                res.statusCode=302
-                res.setHeader('Location','/')
-                //upar ke dono res.writeHead se bhi ho jaega res.writeHead(statuscode,{})
-                return res.end()
-            })
-        })
-    }
-    else{
-        res.setHeader('Content-Type','text/html')
-        res.write('<html>')
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button>submit</button></input></form></body>')
-        res.write('</html>')
-        res.end()
-    }
-}
+const requestHandler=(req,res)=>{
+    const url=req.url
+    const method=req.method
+    if(url === '/'){
+         fs.readFile("message.txt",'utf8',function(err, data) {
+             if (err) {
+                 console.log(err);
+             }
+         const message=data
+         console.log(message)
+         res.write('<html>')
+         res.write(`<body>${message}</body>`)
+         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button>submit</button></input></form></body>')
+         res.write('</html>')
+         return res.end()
+         })
+     }
+     else if(url === '/message' && method==="POST"){
+         const body=[]
+         req.on('data',(chunk)=>{
+            body.push(chunk)
+         })
+         return req.on('end',()=>{
+             const parsedBody=Buffer.concat(body).toString()
+             const message=parsedBody.split('=')[1]
+             fs.writeFile('message.txt', message, err=>{
+                 res.statusCode=302
+                 res.setHeader('Location','/')
+                 //upar ke dono res.writeHead se bhi ho jaega res.writeHead(statuscode,{})
+                 return res.end()
+             })
+         })
+     }
+     else{
+         res.setHeader('Content-Type','text/html')
+         res.write('<html>')
+         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button>submit</button></input></form></body>')
+         res.write('</html>')
+         res.end()
+     }
+ }
 
-module.exports = requestHandlers
-
-// module.exports={
-//     handler: requestHandlers,
-//     sometext: 'some hard coded text'
-// }
-
-
-//exports.Handlers = requestHandlers
-//exports.sometext = 'Some hard coded text'
+ module.exports = requestHandler
